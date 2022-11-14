e14c8f02c8a59b31bf7a0b0e3cb524c812e82f68
leemouse03 <114402750+leemouse03@users.noreply.github.com>
2022-11-14 16:26:19 +0900

    week 12
    1번은 재귀함수 사용해서 어떻게 하는지 
    2번은 if문 까지만 했고,,
    3번은 음 
    4번은,,,,ㅎ 
    죄송합니다 모르겠습니다ㅠㅠ

---
+++ 알파벳 찾기.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    char word[100] = { 0 };
+    scanf("%s", word);
+    
---
+++ 피보나치.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int fibo(int n) {
+    if (n >= 2)
+        return fibo(n - 1) + fibo(n - 2);
+    else if (n == 1) return 1;
+    else return 0;
+}
+ 
+    return 0;
+}
+
